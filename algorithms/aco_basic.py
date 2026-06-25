fdf3d9dac9508680493e029d97fb5869b8b058f8
Cuong512 <quoccuong9a1ccl@gmail.com>
2026-06-25 07:44:21 +0700

    Update aco_basic

--- algorithms/aco_basic.py
+++ algorithms/aco_basic.py
@@ -12,15 +12,15 @@ class Graph:
     def __init__(self, env: DeliveryNetwork, config: Config):
         self.cfg = config
         self.env = env
-        self.adjacency_map = self.create_adjacency_map()
-        self.pheromone_map = self.create_pheromone_map()
-        self.demand_map = {}
+        self.adjacency_map = self.create_adjacency_map() # ma trận khoảng cách
+        self.pheromone_map = self.create_pheromone_map() # ma trận mùi hương
+        self.demand_map = {} # lưu khối lượng hàng hóa
         self.demand_map[self.cfg.DEPOT_ID] = 0
         for i in self.env.get_delivery().keys():
             self.demand_map[i] = self.env.get_delivery()[i]['vol'] # gía trị hàng hóa của khách hàng i
             
     def create_adjacency_map(self) -> Dict[int, Dict[int, float]]:
-        adjacency_map = {} #tạo dict truy xuất khoảng cách dễ hơn thay vì numoy
+        adjacency_map = {} #tạo dict lồng nhau để dễ truy xuất, ví dụ từ 5->8 là dicr[5][8]
         nodes = list(sorted(self.env.delivery_info.keys())) # sắp xếp vị trí các nodes tăng dần
         nodes.insert(0,0)
         
@@ -51,11 +51,11 @@ class Graph:
                 pheromone_map[node_2][node_1] = pheromone_init
         return pheromone_map
 
-    def update_pheromone_map(self, best_solution):
+    def update_pheromone_map(self, best_solution): # hàm update mùi | 4.2 công thức 4
         nodes = list(sorted(self.pheromone_map.keys()))
         for index_1, node_1 in enumerate(nodes):
             for node_2 in nodes[index_1 + 1:]:
-                new_value = max(round((1 - self.cfg.RHO) * self.pheromone_map[node_1][node_2], 5), 1e-10) # update mùi mới, lấy max để đẩm bảo mùi != 0
+                new_value = max(round((1 - self.cfg.RHO) * self.pheromone_map[node_1][node_2], 5), 1e-10) # update mùi mới sau khi bay hơi, lấy max để đẩm bảo mùi != 0
                 self.pheromone_map[node_1][node_2] = new_value
                 self.pheromone_map[node_2][node_1] = new_value
 
@@ -63,7 +63,7 @@ class Graph:
         for route in best_solution.routes:
             edges = [(route[index], route[index + 1]) for index in range(0, len(route) - 1)] # [0, 5, 8, 0] -> [(0, 5), (5, 8), (8, 0)]
             for edge in edges:
-                self.pheromone_map[edge[0]][edge[1]] += (self.cfg.RHO * pheromone_increase)
+                self.pheromone_map[edge[0]][edge[1]] += (self.cfg.RHO * pheromone_increase) # thưởng mùi mới
                 self.pheromone_map[edge[1]][edge[0]] += (self.cfg.RHO * pheromone_increase)
 
 class Ant:
@@ -77,7 +77,7 @@ class Ant:
         self.vehicle = 0 # Bát dầu bằng xe 0
         self.reset_state()
 
-    def get_available_nodes(self, current_node):
+    def get_available_nodes(self, current_node): # dùng để xem từ current_node có đi đến điểm tiếp theo được không | mục 2 trang 42
         allowed_by_time_max = [node for node in self.nodes_left if (self.tour_time[self.vehicle]+self.graph.adjacency_map[current_node][node]+self.env.delivery_info.get(node)['crowd_cost']) <= self.env.delivery_info.get(node)['time_window_max']]
         allowed_by_capacity_time_max_min = [node for node in allowed_by_time_max if self.capacity[self.vehicle] >= self.graph.demand_map[node]]
         return allowed_by_capacity_time_max_min # mảng chứa các khách hàng có thể đến kịp lúc và có thể chứa hàng
@@ -88,16 +88,16 @@ class Ant:
             return None
         return np.random.choice(available_nodes) # Khách đầu random để tăng tính khám phá
 
-    def select_next_delivery(self, current_node):
+    def select_next_delivery(self, current_node): # mục 4.1
         available_nodes = self.get_available_nodes(current_node)
         if not available_nodes:
             return None
                         
         scores = [] # Tính điểm
         for node in available_nodes:
-            tau = pow(self.graph.pheromone_map[current_node][node], self.cfg.ALPHA)
-            eta = pow(1 / self.graph.adjacency_map[current_node][node], self.cfg.BETA)
-            scores.append(tau * eta)
+            tau = pow(self.graph.pheromone_map[current_node][node], self.cfg.ALPHA) # nồng độ mùi
+            eta = pow(1 / self.graph.adjacency_map[current_node][node], self.cfg.BETA) # càng gần càng cao
+            scores.append(tau * eta) 
 
         q = random.random() # random 1 số bất kì
         
@@ -107,7 +107,7 @@ class Ant:
         else:
             denominator = sum(scores)
             probabilities = [score / denominator for score in scores] # tính tỷ lệ phần trăm của score đó
-            next_delivery = np.random.choice(available_nodes, p=probabilities) # quay ngẫu nhiên dựa trên trọng số
+            next_delivery = np.random.choice(available_nodes, p=probabilities) # quay ngẫu nhiên dựa trên trọng số (list)
         
         return next_delivery
 
@@ -127,7 +127,7 @@ class Ant:
         for vehicle in range(0, self.env.n_vehicles):
             self.vehicle = vehicle
             self.routes.append([self.cfg.DEPOT_ID]) #cho điểm bắt đầu vào mảng đầu tiên
-            current_node = self.routes[self.vehicle][-1]
+            current_node = self.routes[self.vehicle][-1] # vị trí hiện tại là vị trí cuối cùng trong route
             available_nodes = self.get_available_nodes(current_node) #Tìm những khách hàng có thể giao
             if available_nodes:
                 first_delivery = np.random.choice(available_nodes) # chọn ngẫu nhiên khách hàng đầu tiên
@@ -141,14 +141,14 @@ class Ant:
                 current_node = self.routes[self.vehicle][-1]
                 
                 if len(self.routes[self.vehicle])>2 and current_node==self.cfg.DEPOT_ID: # Xe đã giao xong và đang ở bãi xuất phát nên không cần đi tiếp
-                    continue
+                    continue # chuyển sang xe tiếp theo
                 else:
-                    next_delivery = self.select_next_delivery(current_node)
-                    if (next_delivery is None):
+                    next_delivery = self.select_next_delivery(current_node) # chọn khách hàng giao tiếp theo
+                    if (next_delivery is None): # nếu xe không thể đi giao tiếp
                         # Vá lỗi infinite loop (Chỉ đi về kho nếu xe đang không ở kho)
                         if current_node != self.cfg.DEPOT_ID:
-                            self.move_to_delivery(current_node, self.cfg.DEPOT_ID)
-                            moved_this_turn = True
+                            self.move_to_delivery(current_node, self.cfg.DEPOT_ID) # đi về kho
+                            moved_this_turn = True # xe này có di chuyển
                     else:
                         self.move_to_delivery(current_node, next_delivery)
                         moved_this_turn = True
@@ -161,7 +161,7 @@ class Ant:
             self.vehicle = vehicle
             if self.routes[self.vehicle][-1] != self.cfg.DEPOT_ID:
                 self.move_to_delivery(self.routes[self.vehicle][-1], self.cfg.DEPOT_ID)      
-        return Solution(self.routes, sum(self.total_path_cost)) # tổng hợp lại và đưa lên lớp Graph để chấm điểm và so sánh
+        return Solution(self.routes, sum(self.total_path_cost)) # tổng hợp lại để chấm điểm và so sánh
 
     def reset_state(self):
         self.capacity = [self.env.get_vehicles()[i]['capacity'] for i in range(0, self.env.n_vehicles)] # phục hồi lại trọng tải từng xe
@@ -179,7 +179,7 @@ class Solution:
 def run(cfg: Config, verbose: bool = True) -> Tuple[Solution, List[Solution]]:
     env = DeliveryNetwork(cfg)
     graph = Graph(env, cfg) 
-    ants = [Ant(graph, cfg) for i in range(0, cfg.NUM_ANTS)]
+    ants = [Ant(graph, cfg) for i in range(0, cfg.NUM_ANTS)] # đẻ ra 100 con sử dụng chung 1 bản đồ Graph
 
     best_solution = None
     all_solutions = []
--- envs/deliveryNetwork.py
+++ envs/deliveryNetwork.py
@@ -5,28 +5,28 @@ from scipy import spatial
 class DeliveryNetwork():
     def __init__(self, settings):
         super(DeliveryNetwork, self).__init__()
-        self.settings = settings
+        self.settings = settings # vpconfig
 
         self.conv_time_to_cost = settings.conv_time_to_cost
         self.n_deliveries = settings.n_deliveries
         self.n_vehicles = settings.n_vehicles
         self.depot = settings.depot
 
-        self.delivery_info = {}
-        points = [self.depot]
+        self.delivery_info = {} # Tạo dcit để chứa thông tin khách hàng
+        points = [self.depot] # Mảng tọa độ
 
         np.random.seed(42)
 
         for i in range(self.n_deliveries):
             lat = self.depot[0] + np.random.uniform(-0.1, 0.1)
-            lng = self.depot[1] + np.random.uniform(-0.1, 0.1)
+            lng = self.depot[1] + np.random.uniform(-0.1, 0.1) # Tạo kinh độ, vĩ độ của từng khách hàng ngẫu nhiên dựa trên depot_id
             points.append([lat, lng])
 
-            vol = 1 
+            vol = 1 # mặc định mỗi khách hàng 1 kiện hàng
             
-            dist_to_depot = math.sqrt((lat - self.depot[0])**2 + (lng - self.depot[1])**2)
-            time_window_min = dist_to_depot * 10
-            time_window_max = time_window_min + 100000 
+            dist_to_depot = math.sqrt((lat - self.depot[0])**2 + (lng - self.depot[1])**2) # Tính khoảng cách so với depot, vĩ độ = x, tung độ = y
+            time_window_min = dist_to_depot * 10 # thời gian dự kiến giao sớm nhất
+            time_window_max = time_window_min + 100000 # thời gian dự kiến giao trễ nhất
 
             self.delivery_info[i + 1] = {
                 'id': i + 1,
@@ -35,38 +35,38 @@ class DeliveryNetwork():
                 'vol': vol,
                 'time_window_min': time_window_min,
                 'time_window_max': time_window_max,
-                'crowd_cost': 0 
+                'crowd_cost': 0 # chi phí phát sinh
             }
 
-        self.distance_matrix = spatial.distance_matrix(points, points)
+        self.distance_matrix = spatial.distance_matrix(points, points) # tính khoảng cách giữa các điểm (khách hàng) -> tạo ra mảng 2c 101*101
 
-        self.vehicles = []
+        self.vehicles = [] # mảng lưu sức chứa và chi phí xuất bãi của từng xe
         for i in range(self.n_vehicles):
             self.vehicles.append({
                 'capacity': settings.vols_vehicles[i],
                 'cost': settings.costs_vehicles[i]
             })
 
-    def get_delivery(self):
+    def get_delivery(self): # getter
         return self.delivery_info
 
-    def get_vehicles(self):
+    def get_vehicles(self): # getter
         return self.vehicles
 
-    def evaluate_VRP(self, VRP_solution):
+    def evaluate_VRP(self, VRP_solution): # hàm chấm điểm lộ trình; VRP_solution là mảng 2c lưu lộ trình của từng xe
         usage_cost = 0 # chi phí xuất bãi
         for k in range(self.n_vehicles):
-            if len(VRP_solution[k]) > 0:
+            if len(VRP_solution[k]) > 0: # nếu lộ trình xe đó không rỗng thì cộng thêm chi phí xuất bãi
                 usage_cost += self.vehicles[k]['cost']
                 
         travel_cost = 0 # chi phí giao hàng
-        for k in range(self.n_vehicles):
-            tour_time = 0
-            for i in range(1, len(VRP_solution[k])-1): # bỏ trạm xuất phát
+        for k in range(self.n_vehicles): # duyệt qua từng xe
+            tour_time = 0 # thời gian chạy
+            for i in range(1, len(VRP_solution[k])-1): # duyệt qua từng trạm của xe, bỏ trạm xuất phát
                 tour_time += self.distance_matrix[VRP_solution[k][i - 1], VRP_solution[k][i]] # tính khoảng cách giữa trạm trước và hiện tại | mặc định v = 1
                 tour_time = max(tour_time, self.delivery_info[VRP_solution[k][i]]['time_window_min']) # nếu xe đến quá sớm thì ép lên thời gian khách hàng mở cửa
                 if tour_time > self.delivery_info[VRP_solution[k][i]]['time_window_max']: # nếu trễ thì loại
                     raise Exception('Too Late for Delivery: ', VRP_solution[k][i])
             travel_cost += self.conv_time_to_cost * tour_time # đổi thời gian sang chi phí
             
-        return usage_cost + travel_cost
+        return usage_cost + travel_cost # return về tổng chi phí của lần duyệt này
